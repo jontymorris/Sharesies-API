e958aa90897162da7f8f75266d07dcf186e571a9
Jonty <jonty.morris@yahoo.co.nz>
2020-04-01 13:16:24 +1300

    1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sharesies',
-    version='1.0.0',
+    version='1.1.0',
     description='Unoffical Python API for Sharesies NZ',
     url='https://github.com/jontymorris/Sharesies-API',
     author='Jonty Morris',
